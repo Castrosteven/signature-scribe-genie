f613b7bed6fb88f30001b02784a7c610cb12df3f
Steven Castro <steve@Stevens-MacBook-Air.local>
2025-04-01 23:34:57 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/signature-scribe-genie" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
